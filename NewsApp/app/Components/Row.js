543781c6efc92377bcf4d793920d661a5a42e647
admin <admin@USCS-Mac158.local>
2018-10-19 13:52:57 -0700

    Initial Commit

---
+++ NewsApp/app/Components/Row.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class Row extends Component {
+
+    render(){
+        return (
+            <Text> Some Text !!!</Text>
+        );
+    }
+}
+
+export default Row;
---
+++ NewsApp/app/LandingComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Row from './Components/Row';
+
+class LandingComponent extends Component {
+
+    render(){
+        return (
+            <View>
+                <Row />
+            </View>
+        );
+    }
+}
+
+export default LandingComponent;
